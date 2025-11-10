8fe5a7b33487416f031deb209ed838c4e6d0b885
Victor Mendes <victormendesgyn774@gmail.com>
2025-11-10 19:47:31 -0300

    Mudança dos ingredientes e adição de um novo produto

--- app.py
+++ app.py
@@ -8,27 +8,34 @@ def home():
     items = [
         {
             "nome": "Batburger",
-            "descricao": "Hambúrguer negro com queijo cheddar e molho secreto do Batman",
+            "descricao": "Pão de brioche negro, 180g de um blend de costela e peito bovino, Queijo cheddar inglês maturado e Maionese de alho negro defumado",
             "preco": "R$ 25,00",
             "imagem": "batman.jpg"
         },
         {
-            "nome": "Super Shake",
-            "descricao": "Milkshake azul e vermelho com chantilly",
+            "nome": "Super-Shake",
+            "descricao": "Sorvete de baunilha batido com calda de blue raspberry e o copo é forrado com calda de morango",
             "preco": "R$ 18,00",
             "imagem": "superman.jpg"
         },
         {
             "nome": "Laço da Verdade",
-            "descricao": "Wrap de frango grelhado com molho dourado da Mulher-Maravilha",
+            "descricao": "Tiras de frango grelhado,espinafre fresco, tomate cereja e queijo feta, tudo enrolado em uma tortilha dourada de milho ou cúrcuma",
             "preco": "R$ 22,00",
             "imagem": "wonderwoman.jpg"
         },
         {
-            "nome": "Velocidade Escarlate",
-            "descricao": "Torta de morango com cobertura flamejante",
+            "nome": "Velocista Escarlate",
+            "descricao": "Base crocante de biscoito, um recheio de cheesecake leve, e morangos frescos fatiados. Uma calda de morango brilhante cobrindo tudo, finalizada com raspas finas de casca de laranja ",
             "preco": "R$ 16,00",
             "imagem": "Flash.jpg"
+        },
+
+        {
+            "nome": "Energia Esmeralda",
+            "descricao": "Uma bebida verde vibrante pode ser um suco ou smoothiefeita com abacaxi, hortelã fresca e limão, ganhando sua cor intensa de um toque de espinafre",
+            "preco": "R$ 10,00",
+            "imagem": "lanternaverde.jpg"
         }
     ]
     return render_template('index.html', items=items)
